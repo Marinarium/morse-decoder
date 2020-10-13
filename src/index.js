294cf756b5cefa10a92226304c7540076c378ca2
Marinarium <schwmk@gmail.com>
2020-10-13 23:55:13 +0300

    init: morse decoder

--- src/index.js
+++ src/index.js
@@ -39,6 +39,40 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    let str = '';
+	
+	for (let j = 0; j <= expr.length; j++) {
+		let tempStr = expr.slice(0, 10);
+		let tempSymbols = '';
+		let morseSymbols = '';
+		
+		if (tempStr === '**********') {
+			str += ' ';
+		}
+		
+		for(let i = 10; i > 1; i--) {
+			tempSymbols = tempStr.slice(0, 2);
+			if (tempSymbols == 10) {
+				morseSymbols += '.';
+				tempStr = tempStr.slice(2);
+			} else if (tempSymbols == 11) {
+				morseSymbols += '-';
+				tempStr = tempStr.slice(2);
+			} else {
+				tempStr = tempStr.slice(1);
+			}
+		}
+		
+		if (morseSymbols) {
+			str += MORSE_TABLE[morseSymbols];
+		}
+		
+		
+		
+		expr = expr.slice(10);
+    }
+    
+    return str;
 }
 
 module.exports = {
